5fa2a5367462a389c599ce379d1e4611cf33c4d6
AndreySavchenkov <sannndvik16@gmail.com>
2024-04-16 09:19:56 +0200

    feat: styled about me section

---
+++ app/components/AboutSection.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+
+enum TabsEnum {
+  SKILLS = "skills",
+  EDUCATION = "education",
+  EXPERIENCE = "experience",
+}
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: TabsEnum.SKILLS,
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>JavaScript</li>
+        <li>Typescript</li>
+        <li>React</li>
+        <li>NextJS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: TabsEnum.EDUCATION,
+    content: (
+      <ul className="list-disc pl-2">
+        <li>IT Incubator</li>
+        <li>RS School</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experience",
+    id: TabsEnum.EXPERIENCE,
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Purrweb</li>
+      </ul>
+    ),
+  },
+];
+
+function AboutSection() {
+  const [tab, setTab] = useState<TabsEnum>(TabsEnum.SKILLS);
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: TabsEnum) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white ">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 ">
+        <Image
+          src="/images/about-image.png"
+          alt="about image"
+          width={500}
+          height={500}
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a frontend developer with some knowledge backend developing I
+            am a frontend developer with some knowledge backend developing I am
+            a frontend developer with some knowledge backend developing I am a
+            frontend developer with some knowledge backend developing I am a
+            frontend developer with some knowledge backend developing I am a
+            frontend developer with some knowledge backend developing
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange(TabsEnum.SKILLS)}
+              active={tab === TabsEnum.SKILLS}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange(TabsEnum.EDUCATION)}
+              active={tab === TabsEnum.EDUCATION}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange(TabsEnum.EXPERIENCE)}
+              active={tab === TabsEnum.EXPERIENCE}
+            >
+              Experience
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab)?.content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
---
+++ app/components/TabButton.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+};
+
+function TabButton({ active, selectTab, children }: Props) {
+  const buttonClasses = active
+    ? "text-white border-b border-purple-500"
+    : "text-[#ADB7BE]";
+
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
+}
+
+export default TabButton;
