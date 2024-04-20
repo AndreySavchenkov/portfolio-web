11533c4657fbc9f1048dc399c74eca746d9916ef
AndreySavchenkov <sannndvik16@gmail.com>
2024-04-20 18:50:03 +0200

    fix: fix navbar styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -25,8 +25,8 @@ function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+    <nav className="fixed top-0 left-0 border border-[#33353F] right-0 z-20 bg-[#121212] bg-opacity-100">
+      <div className="flex flex-wrap lg:py-4 items-center justify-between mx-auto px-4 py-2">
         <Link
           className="text-2xl md:text-5xl text-white font-semibold"
           href={"/"}
